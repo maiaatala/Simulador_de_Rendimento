b02877f97ef4c37ece0311a53b54bbf1d9fd8347
maiaatala <ana.atala@unemat.br>
2021-05-29 10:13:41 -0400

    inicial commit
    add files

---
+++ Simulador_de_Rendimento.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h> //for system ('cls');
+#include <math.h>   //for pow
+
+/* programa para calcular
+valor acumulado em rendimento
+sob depósitos mensais, como a polpança */
+
+float calculo_valor_ac(float aplicacao, float tx, int m)
+{
+    float   va;
+    //formula utilizada para calcular o valor final da aplicação
+    va  =   (aplicacao*(pow((1+tx),m)-1))/tx;
+
+    return  (va);
+}
+
+void simulador_de_rendimento()
+{
+    //declaração de variaveis
+    float   valor_ac, v_aplicacao, taxa;
+    int     meses;
+
+    //entrada de dados
+    printf  ("\nInsira o valor a ser aplicado mensalmente: ");
+    scanf   ("%f", &v_aplicacao);
+    printf  ("Insira a taxa mensal em %% (o valor sera dividido por 100): ");
+    scanf   ("%f", &taxa);
+    printf  ("Insira a quantidade de meses da aplicacao: ");
+    scanf   ("%i", &meses);
+
+    //processamento de dados
+    taxa        = taxa/100;
+    valor_ac    = calculo_valor_ac(v_aplicacao,taxa,meses);
+
+    //saida de dados
+    printf  ("\n-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-\n");
+    printf  ("O valor acumulado e: %5.2f reais\n", valor_ac);
+    printf  ("O valor do rendimento e de: %5.2f reais \n", valor_ac - v_aplicacao*meses);
+    printf  ("-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-\n");
+
+}
+
+void credits()
+{
+    //printf  (":--------------+--------------------:\n");
+    printf  ("| Obrigada por utilizar o programa. |\n");
+    printf  (":--------------+--------------------:\n");
+    printf  ("| Autor:       | Ana Atala          |\n");
+    printf  (":--------------+--------------------:\n");
+    printf  ("| Data:        | Maio 28, 2021      |\n");
+    printf  (":--------------+--------------------:\n");
+    printf  ("| Materia:     | Lab Programacao I  |\n");
+    printf  (":--------------+--------------------:\n");
+    printf  ("| Faculdade:   | Unemat             |\n");
+    printf  (":--------------+--------------------:\n\n");    
+}
+
+int main()
+{
+    //declaração de variaveis
+    char    escolha;
+
+    //intro do programa
+    //system  ("cls"); //lipar tela do cmd;
+    printf  ("\nPrograma para calcular o valor acumulado em aplicacoes mensais com rendimento %% ao mes\n");
+    printf  ("--------------------------------------------------------------------------------------\n");
+
+    //Loop do programa
+    /* loop repete enquanto o usua esolher que sim ('s')
+    possibilitando a reutilizaçao do programa */
+    do
+    {
+        simulador_de_rendimento();
+        printf  ("\nDeseja simular outros valores para aplicacao mensal? (s/n): ");
+        scanf   (" %c", &escolha);  //o espaco é para o programa ignorar white spaces
+        printf  ("\n-------------------------------------\n");
+    }while((escolha == 's')||(escolha == 'S')); //C is case sensitive
+
+    //Credits
+    //printf  ("Obrigada por utilizar o programa. \n\nAutor:\t\tAna Atala  \nData:\t\tMaio 20, 2021  \nMateria:\tLab Programacao I  \nFaculdade:\tUnemat  \n\n");
+    credits();
+    system  ("pause");
+    return  (0);
+}
---
+++ readME.md
@@ -0,0 +1,38 @@
+# Simulador de rendimento <!-- omit in toc -->
+
+Programa proposto durante aula de laboratório de programação I
+
+***
+
+## 1. sumário <!-- omit in toc -->
+
+- [2. Introdução](#2-introdução)
+- [3. Questão](#3-questão)
+- [4. Informaçoes sobre o programa](#4-informaçoes-sobre-o-programa)
+
+***
+
+## 2. Introdução
+
+O programa tem a intenção de calcular qual é o valor acumulado após uma aplicação mensal, como o valor acumulado apos depósitos mensais na poupança
+
+Linguagem Utilizada: C
+
+Data dos programas: Maio 2021.
+
+Autora: Ana C Maia Atala. :e-mail: @ ana.atala@unemat.br
+
+Faculdade: Unemat :school:
+
+***
+
+## 3. Questão
+
+![A questão proposta](https://cdn.discordapp.com/attachments/838789007064301578/847485624969986048/unknown.png)
+
+***
+
+## 4. Informaçoes sobre o programa
+
+O Programa requer como entrada o valor do depósito, a taxa mensal e a quantidade de meses que os depósitos vao durar.
+Além disso, o program apresenta um loop `do{ ... }while( )` para possibilitar a reutilizaçãodo mesmo
